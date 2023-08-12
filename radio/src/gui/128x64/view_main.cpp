Sled-Aero/edgetx

refactor: watchdog and abnormal reboot drivers

radio/src/gui/128x64/view_main.cpp
@@ -59,6 +59,22 @@
 #define CLOCK_X       53
 #define CLOCK_Y       57
 
+#if defined(ASTERISK) || !defined(WATCHDOG) || defined(LOG_TELEMETRY) || \
+    defined(LOG_BLUETOOTH) || defined(DEBUG_LATENCY)
+
+static bool isAsteriskDisplayed() {
+  return true;
+}
+
+#else
+
+#include "hal/abnormal_reboot.h"
+
+static bool isAsteriskDisplayed() {  
+  return UNEXPECTED_SHUTDOWN();
+}
+#endif
+
 void drawExternalAntennaAndRSSI()
 {
 #if defined(INTERNAL_MODULE_PXX1) && defined(EXTERNAL_ANTENNA)

radio/src/gui/212x64/view_main.cpp
@@ -76,6 +76,22 @@ const unsigned char icons[]  = {
 #define ICON_REBOOT   91, 11
 #define ICON_ALTITUDE 102, 9
 
+#if defined(ASTERISK) || !defined(WATCHDOG) || defined(LOG_TELEMETRY) || \
+    defined(LOG_BLUETOOTH) || defined(DEBUG_LATENCY)
+
+static bool isAsteriskDisplayed() {
+  return true;
+}
+
+#else
+
+#include "hal/abnormal_reboot.h"
+
+static bool isAsteriskDisplayed() {  
+  return UNEXPECTED_SHUTDOWN();
+}
+#endif
+
 void doMainScreenGraphics()
 {
   int16_t calibStickVert = calibratedAnalogs[ADC_MAIN_LV];

kobakirill/RU

radio/src/gui/128x64/view_main.cpp
@@ -59,6 +59,22 @@
 #define CLOCK_X       53
 #define CLOCK_Y       57
 
+#if defined(ASTERISK) || !defined(USE_WATCHDOG) || defined(LOG_TELEMETRY) || \
+    defined(LOG_BLUETOOTH) || defined(DEBUG_LATENCY)
+
+static bool isAsteriskDisplayed() {
+  return true;
+}
+
+#else
+
+#include "hal/abnormal_reboot.h"
+
+static bool isAsteriskDisplayed() {
+  return UNEXPECTED_SHUTDOWN();
+}
+#endif
+
 void drawExternalAntennaAndRSSI()
 {
 #if defined(INTERNAL_MODULE_PXX1) && defined(EXTERNAL_ANTENNA)

radio/src/gui/212x64/view_main.cpp
@@ -76,6 +76,22 @@ const unsigned char icons[]  = {
 #define ICON_REBOOT   91, 11
 #define ICON_ALTITUDE 102, 9
 
+#if defined(ASTERISK) || !defined(USE_WATCHDOG) || defined(LOG_TELEMETRY) || \
+    defined(LOG_BLUETOOTH) || defined(DEBUG_LATENCY)
+
+static bool isAsteriskDisplayed() {
+  return true;
+}
+
+#else
+
+#include "hal/abnormal_reboot.h"
+
+static bool isAsteriskDisplayed() {  
+  return UNEXPECTED_SHUTDOWN();
+}
+#endif
+
 void doMainScreenGraphics()
 {
   int16_t calibStickVert = calibratedAnalogs[ADC_MAIN_LV];