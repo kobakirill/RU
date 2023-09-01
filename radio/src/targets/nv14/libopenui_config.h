mshonak/edgetx

chore: convert LTDC to HAL/LL

radio/src/targets/nv14/libopenui_config.h
@@ -25,7 +25,7 @@
 #include "libopenui_defines.h"
 
 #include "colors.h"
-#include "board.h"
+#include "hal.h"
 #include "keys.h"
 
 enum FontIndex

kobakirill/RU

radio/src/targets/nv14/libopenui_config.h
@@ -21,6 +21,7 @@
 
 #pragma once
 
+#include "board.h"
 constexpr uint32_t ALERT_FRAME_TOP =               70;
 constexpr uint32_t ALERT_FRAME_HEIGHT =            (LCD_H - 2 * ALERT_FRAME_TOP);
 constexpr uint32_t ALERT_BITMAP_TOP =              ALERT_FRAME_TOP + 15;